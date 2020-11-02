3c09123656da1a5054e28c970d227d0eefa2d01d
8-Lambda-8 <j.wasle111@gmail.com>
2020-11-02 12:55:04 +0100

    midi stuff

--- code.py
+++ code.py
@@ -1,4 +1,5 @@
 import time
+from array import *
 import board
 
 import touchio
@@ -6,28 +7,21 @@ import pulseio
 from analogio import AnalogIn
 from digitalio import DigitalInOut, Direction, Pull
 
-import usb_hid
-from adafruit_hid.consumer_control import ConsumerControl
-from adafruit_hid.consumer_control_code import ConsumerControlCode
-
 import usb_midi
-#import adafruit_lis3dh
 import adafruit_midi
+from adafruit_midi.midi_message     import note_parser
 from adafruit_midi.note_on          import NoteOn
+from adafruit_midi.note_off         import NoteOff
 from adafruit_midi.control_change   import ControlChange
 from adafruit_midi.pitch_bend       import PitchBend
-
-#DefineTouch Thresholds
-touchThresholds = [3000,3000,3000,3000,3000,3600]
-
-buttonLedDelay = 0.2
-
 
 #####INPUTS
 
 #init touch buttons
 pads = [board.A0,board.A1,board.A2,board.A3,board.A4,board.A5]
 touchpads = [touchio.TouchIn(pad) for pad in pads]
+touchThresholds = [3000,3000,3000,3000,3000,3600]
+for idx, touchpad in enumerate(touchpads): touchpad.threshold = touchThresholds[idx]
 del pads
 
 #init Fader
@@ -50,11 +44,13 @@ del btnPins
 #init led lines
 touchLedPins = [board.D0,board.D1,board.D2,board.D3,board.D4,board.D5]
 touchLeds = [initIO(touchLedPin,Direction.OUTPUT) for touchLedPin in touchLedPins]
+for touchLed in touchLeds: touchLed.value = True # Turn Off
 del touchLedPins
 
 #init Btn Leds
 btnLedPins = [board.D10,board.D11,board.D12,board.D13]
 btnLeds = [initIO(btnLedPin,Direction.OUTPUT) for btnLedPin in btnLedPins]
+for btnLed in btnLeds: btnLed.value = True # Turn Off
 del btnLedPins
 
 del initIO
@@ -65,11 +61,30 @@ ledG = pulseio.PWMOut(board.D15, frequency=5000, duty_cycle=0)
 ledB = pulseio.PWMOut(board.D16, frequency=5000, duty_cycle=0)
 
 
-#uncomment these if keyboard library is installed. See top comment
-time.sleep(1)  # Sleep for a bit to avoid a race condition on some systems
-cc = ConsumerControl(usb_hid.devices)
+time.sleep(1)
 
-for btnLed in btnLeds: btnLed.value = True
+#### MIDI
+
+midi_channel = 1
+midi = adafruit_midi.MIDI(midi_out=usb_midi.ports[1], out_channel=midi_channel-1)
+midi_in = adafruit_midi.MIDI(midi_in=usb_midi.ports[0], in_channel=midi_channel-1)
+
+
+velocity = 127
+
+midiNotes = [[1,2,3,4,5,6],#TouchBtns
+             [7,8,9,10],#TactileBtns
+             [11,12,13,14],#Fader
+             [15,16,17,18],#FaderReset
+             [19,20,21]]#LogoRGB Feedback
+
+print(midiNotes)
+
+keydownTouch = [False] * len(touchpads)
+keydownBtn = [False] * len(btns)
+FaderLast = [0]*len(faders)
+FaderLastRaw = [0]*len(faders)
+FaderOverride = [0]*len(faders)
 
 def LogoColor(r,g,b):
     #print(str(r)+","+str(g)+","+str(b))
@@ -83,113 +98,80 @@ def LogoColor(r,g,b):
 def clamp(n, minn, maxn):
     return max(min(maxn, n), minn)
 
+def index_2d(myList, v):
+    for i, x in enumerate(myList):
+        if v in x:
+            return [i, x.index(v)]
+
+
 #def getLogoColor()
 
 LogoColor(0,0,0)
-time.sleep(0.3)
+time.sleep(0.2)
 LogoColor(255,0,0)
-time.sleep(0.3)
+time.sleep(0.2)
 LogoColor(0,255,0)
-time.sleep(0.3)
+time.sleep(0.2)
 LogoColor(0,0,255)
-time.sleep(0.3)
+time.sleep(0.2)
 
 
 print("starting...")
 #important: LED is active when False!
 #important: capacitive values might need adjustment depending on your procedural design
 
-faderLogo = False
-
 while True:
-    if touchpads[0].raw_value > touchThresholds[0]:
-        #print("1: "+str(touch1.raw_value))
-        #cc.send(ConsumerControlCode.VOLUME_INCREMENT) #uncomment if keyboard library is installed
-        touchLeds[0].value = False
-        LogoColor(0,0,0)
-        time.sleep(buttonLedDelay)
 
-    else:
-        touchLeds[0].value = True
+    msg = midi_in.receive()
+    if isinstance(msg, NoteOn) or isinstance(msg, NoteOff):
+        #print(str(msg.note)+" "+str(msg.velocity))
 
-    if touchpads[1].raw_value > touchThresholds[1]:
-        #print("2: "+str(touch2.raw_value))
-        #cc.send(ConsumerControlCode.VOLUME_DECREMENT)  #uncomment if keyboard library is installed
-        touchLeds[1].value = False
-        LogoColor(255,0,0)
-        time.sleep(buttonLedDelay)
+        index = index_2d(midiNotes,msg.note)
+        #print(index)
 
-    else:
-        touchLeds[1].value = True
-
-    if touchpads[2].raw_value > touchThresholds[2]:
-        #print("3: "+str(touch3.raw_value))
-        #cc.send(ConsumerControlCode.MUTE)  #uncomment if keyboard library is installed
-        touchLeds[2].value = False
-        LogoColor(0,255,0)
-        time.sleep(buttonLedDelay)
-
-    else:
-        touchLeds[2].value = True
-        
-    if touchpads[3].raw_value > touchThresholds[3]:
-        #print("4: "+str(touch4.raw_value))
-        #cc.send(ConsumerControlCode.MUTE)  #uncomment if keyboard library is installed
-        touchLeds[3].value = False
-        LogoColor(0,0,255)
-        time.sleep(buttonLedDelay)
-    else:
-        touchLeds[3].value = True
-
-    if touchpads[4].raw_value > touchThresholds[4]:
-        #print("5: "+str(touch5.raw_value))
-        #cc.send(ConsumerControlCode.MUTE)  #uncomment if keyboard library is installed
-        touchLeds[4].value = False
-        LogoColor(255,255,255)
-        time.sleep(buttonLedDelay)
-    else:
-        touchLeds[4].value = True
-
-    if touchpads[5].raw_value > touchThresholds[5]:
-        #print("6: "+str(touch6.raw_value))
-        #cc.send(ConsumerControlCode.MUTE)  #uncomment if keyboard library is installed
-        touchLeds[5].value = False
-        faderLogo = True
-        time.sleep(buttonLedDelay)
-    else:
-        touchLeds[5].value = True
-
-    #Read Faders
+        if(index[0]==0):#touch
+            touchLeds[index[1]].value = not int(msg.velocity/127)
+        elif(index[0]==1):#Btn
+            btnLeds[index[1]].value = not int(msg.velocity/127)
+        #elif(index[0]==2):#Fader
+        elif(index[0]==4):#Logo
+            if index[1]==0:
+                ledR.duty_cycle = int((msg.velocity) * 65535 / 127)
+            elif index[1]==1:
+                ledG.duty_cycle = int((msg.velocity) * 65535 / 127)
+            elif index[1]==2:
+                ledB.duty_cycle = int((msg.velocity) * 65535 / 127)
 
 
-    if faderLogo:
-        ledR.duty_cycle = clamp(faders[0].value-64,0,65535)
-        ledG.duty_cycle = clamp(faders[1].value-64,0,65535)
-        ledB.duty_cycle = clamp(faders[2].value-64,0,65535)
 
+    for idx, touchpad in enumerate(touchpads):
+        if touchpad.value != keydownTouch[idx]:
+            keydownTouch[idx] = touchpad.value
+            if keydownTouch[idx]:
+                midi.send(NoteOn(midiNotes[0][idx], velocity))
+            else:
+                midi.send(NoteOn(midiNotes[0][idx], 0))  # Using note on 0 for off
 
-    #Read Buttons
+    for idx, btn in enumerate(btns):
+        if btn.value != keydownBtn[idx]:
+            keydownBtn[idx] = btn.value
+            if not keydownBtn[idx]:
+                midi.send(NoteOn(midiNotes[1][idx], velocity))
+            else:
+                midi.send(NoteOn(midiNotes[1][idx], 0))  # Using note on 0 for off
+    
+    for idx, fader in enumerate(faders):
+        faderVal = int(fader.value/512)
+        if faderVal != FaderLast[idx] and abs(FaderLastRaw[idx]-fader.value)>255:
+            FaderLast[idx] = faderVal
+            FaderLastRaw[idx] = fader.value
+            if faderVal>1:
+                midi.send(NoteOn(midiNotes[2][idx], faderVal))
+                if not FaderOverride:
+                    FaderOverride[idx] = False
+                    midi.send(NoteOn(midiNotes[3][idx], 0)) #set Fader override                
 
-    if not btns[0].value:
-        btnLeds[0].value = False
-        time.sleep(buttonLedDelay)
-    else:
-        btnLeds[0].value = True
-
-    if not btns[1].value:
-        btnLeds[1].value = False
-        time.sleep(buttonLedDelay)
-    else:
-        btnLeds[1].value = True
-
-    if not btns[2].value:
-        btnLeds[2].value = False
-        time.sleep(buttonLedDelay)
-    else:
-        btnLeds[2].value = True
-
-    if not btns[3].value:
-        btnLeds[3].value = False
-        time.sleep(buttonLedDelay)
-    else:
-        btnLeds[3].value = True
+            else:
+                FaderOverride[idx] = False
+                midi.send(NoteOn(midiNotes[2][idx], 0))
+                midi.send(NoteOn(midiNotes[3][idx], velocity)) #reset Fader override
